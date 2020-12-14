ab9a1a829affc852129d2c11617114ec68b6efe8
Marco Parrone <marco@marcoparrone.com>
2020-12-14 23:54:58 +0100

    Now use workbox.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-//import * as serviceWorker from './serviceWorker';
 
 var m = document.createElement('meta');
 m.name = 'theme-color';
@@ -10,7 +9,3 @@ m.content = getComputedStyle(document.documentElement).getPropertyValue('--color
 document.head.appendChild(m);
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-//serviceWorker.register(); -- I want to use my custom service worker not the create-react-app one.
-
-navigator.serviceWorker.register('service-worker.js');
