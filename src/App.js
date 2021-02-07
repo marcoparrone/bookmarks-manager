b469d446e2e3f2b9fc27c25946ea70fc48ac4cab
Marco Parrone <marco@marcoparrone.com>
2021-02-07 23:49:25 +0100

    Factored out react-node component.

--- src/App.js
+++ src/App.js
@@ -14,8 +14,6 @@ import {Dialog, open_dialog} from '@marcoparrone/dialog';
 
 import AppWithTopBar from '@marcoparrone/appwithtopbar';
 
-import IconButton from './iconbutton';
-
 import {
   add_node, get_node, change_node_field, delete_node,
   move_node_backward, move_node_forward, move_node_upward, move_node_downward,
@@ -25,79 +23,11 @@ import {
 import EditDialog from './edit-dialog';
 import SettingsDialog from './settings-dialog';
 
+import Node from './react-node';
+
 const defaultText = require ('./en.json');
 
-class Bookmark extends React.Component {
-  render() {
-    let content = [];
-    let count = 0;
-    let element = null;
-    let keyprefix = "Bookmark" + this.props.id;
-    if (this.props.type === 'bookmark') {
-      content.push(<label key={keyprefix}>{this.props.title}&nbsp;
-                            <a href={this.props.url} target="_blank" rel="noreferrer">
-          <IconButton key={keyprefix + "-OpenButton"} label={this.props.text['text_open']} icon='open_in_new' />
-        </a>
-      </label>);
-    } else {
-      content.push(<label key={keyprefix + "Label"}>{this.props.title}: </label>);
-
-      content.push(<br key={keyprefix + "Br"} />);
-
-      if (this.props.children !== undefined && this.props.children !== null && this.props.children !== []) {
-        count = this.props.children.length;
-        for (let i = 0; i < count; i++) {
-          element = this.props.children[i];
-          if (element.visible !== 0) {
-            content.push(<Bookmark
-              id={this.props.id + "." + i.toString()}
-              key={keyprefix + "." + i.toString()}
-              type={element.type}
-              title={element.title}
-              url={element.url}
-              children={element.children}
-              showedit={this.props.showedit}
-              showmove={this.props.showmove}
-              showadd={this.props.showadd}
-              addBookmark={this.props.addBookmark}
-              editBookmark={this.props.editBookmark}
-              movebackwardBookmark={this.props.movebackwardBookmark}
-              moveforwardBookmark={this.props.moveforwardBookmark}
-              moveupwardBookmark={this.props.moveupwardBookmark}
-              movedownwardBookmark={this.props.movedownwardBookmark}
-              text={this.props.text}
-            />);
-          }
-        }
-      }
-
-      if (this.props.showadd === 'yes') {
-        content.push(<IconButton key={keyprefix + "-AddButton"} label={this.props.text['text_add']} icon='add' callback={event => this.props.addBookmark(this.props.id)} />);
-      }
-    }
-
-    if (this.props.showedit === 'yes') {
-      content.push(<IconButton key={keyprefix + "-EditButton"} label={this.props.text['text_edit']} icon='edit' callback={event => this.props.editBookmark(this.props.id)} />);
-    }
-
-    if (this.props.showmove === 'yes') {
-      content.push(<IconButton key={keyprefix + "-BackwardButton"} label={this.props.text['text_move_backward']} icon='keyboard_arrow_left' callback={event => this.props.movebackwardBookmark(this.props.id)} />);
-      content.push(<IconButton key={keyprefix + "-ForwardButton"} label={this.props.text['text_move_forward']} icon='keyboard_arrow_right' callback={event => this.props.moveforwardBookmark(this.props.id)} />);
-      content.push(<IconButton key={keyprefix + "-UpwardButton"} label={this.props.text['text_move_upward']} icon='keyboard_arrow_up' callback={event => this.props.moveupwardBookmark(this.props.id)} />);
-      content.push(<IconButton key={keyprefix + "-DownwardButton"} label={this.props.text['text_move_downward']} icon='keyboard_arrow_down' callback={event => this.props.movedownwardBookmark(this.props.id)} />);
-    }
-
-    return (
-      <div className="mdc-card  mdc-card--outlined" key={keyprefix + "Card"}>
-        <div className="card-body mdc-typography--body2">
-          {content}
-        </div>
-      </div>
-    );
-  }
-}
-
-class BookmarksList extends React.Component {
+class NodesList extends React.Component {
 
   constructor(props) {
     super(props);
@@ -109,23 +39,24 @@ class BookmarksList extends React.Component {
 
     this.state = { bookmarks: this.bookmarks };
 
-    this.deleteBookmark = this.deleteBookmark.bind(this);
+    this.deleteNode = this.deleteNode.bind(this);
 
-    this.addBookmark = this.addBookmark.bind(this);
-    this.editBookmark = this.editBookmark.bind(this);
+    this.addNode = this.addNode.bind(this);
+    this.openNode = this.openNode.bind(this);
+    this.editNode = this.editNode.bind(this);
     this.openSettings = this.openSettings.bind(this);
 
-    this.movebackwardBookmark = this.movebackwardBookmark.bind(this);
-    this.moveforwardBookmark = this.moveforwardBookmark.bind(this);
-    this.moveupwardBookmark = this.moveupwardBookmark.bind(this);
-    this.movedownwardBookmark = this.movedownwardBookmark.bind(this);
+    this.movebackwardNode = this.movebackwardNode.bind(this);
+    this.moveforwardNode = this.moveforwardNode.bind(this);
+    this.moveupwardNode = this.moveupwardNode.bind(this);
+    this.movedownwardNode = this.movedownwardNode.bind(this);
 
     this.handleSettingsChange = this.handleSettingsChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
 
-    this.importBookmarksReaderOnload = this.importBookmarksReaderOnload.bind(this);
-    this.importBookmarks = this.importBookmarks.bind(this);
-    this.exportBookmarks = this.exportBookmarks.bind(this);
+    this.importNodesReaderOnload = this.importNodesReaderOnload.bind(this);
+    this.importNodes = this.importNodes.bind(this);
+    this.exportNodes = this.exportNodes.bind(this);
 
     this.bookmarksListRef = React.createRef();
     this.EditDialogRef = React.createRef();
@@ -151,14 +82,14 @@ class BookmarksList extends React.Component {
     this.i18n = new I18n(() => {this.forceUpdate()});
 
     // Load the bookmarks from localStorage.
-    this.loadBookmarks();
+    this.loadNodes();
   }
 
   componentWillUnmount() {
 
   }
 
-  saveBookmarks() {
+  saveNodes() {
     save_nodes(this.bookmarks, 'bookmarks');
     this.setState({bookmarks: this.bookmarks});
   }
@@ -167,7 +98,7 @@ class BookmarksList extends React.Component {
     change_node_field(this.bookmarks, cursor, 'type', type);
     change_node_field(this.bookmarks, cursor, 'title', title);
     change_node_field(this.bookmarks, cursor, 'url', url);
-    this.saveBookmarks();
+    this.saveNodes();
   }
 
   handleSettingsChange(showedit, showmove, showadd, language) {
@@ -197,7 +128,7 @@ class BookmarksList extends React.Component {
     }
   }
 
-  loadBookmarks() {
+  loadNodes() {
     let bookmarks = load_nodes('bookmarks');
     if (bookmarks) {
       this.bookmarks = bookmarks;
@@ -205,7 +136,7 @@ class BookmarksList extends React.Component {
     }
   }
 
-  addBookmark(cursor) {
+  addNode(cursor) {
     let newbookmark = {
       type: 'bookmark',
       title: '',
@@ -214,11 +145,18 @@ class BookmarksList extends React.Component {
     };
     let newCursor = add_node(this.bookmarks, cursor, newbookmark);
     change_node_field(this.bookmarks, newCursor, 'title', this.i18n.text['text_example_title'] + ' ' + newCursor);
-    this.saveBookmarks();
-    this.editBookmark(newCursor);
+    this.saveNodes();
+    this.editNode(newCursor);
   }
 
-  editBookmark(cursor) {
+  openNode(cursor) {
+    let bookmark = get_node(this.bookmarks, cursor);
+    if (bookmark) {
+      window.open(bookmark.url, '_blank', 'noopener');
+    }
+  }
+
+  editNode(cursor) {
     let bookmark = get_node(this.bookmarks, cursor);
     if (bookmark) {
       this.EditDialogRef.current.updateState(cursor, bookmark.type, bookmark.title, bookmark.url);
@@ -231,40 +169,40 @@ class BookmarksList extends React.Component {
     open_dialog(this.bookmarksListRef, 'settings');
   }
 
-  movebackwardBookmark(cursor) {
+  movebackwardNode(cursor) {
     if (move_node_backward(this.bookmarks, cursor)) {
-      this.saveBookmarks();
+      this.saveNodes();
     }
   }
 
-  moveforwardBookmark(cursor) {
+  moveforwardNode(cursor) {
     if (move_node_forward(this.bookmarks, cursor)) {
-      this.saveBookmarks();
+      this.saveNodes();
     }
   }
 
-  moveupwardBookmark(cursor) {
+  moveupwardNode(cursor) {
     const emptynode = {type: 'bookmark', title: "InvisibleElement", content: "InvisibleContent", visible: 0};
     if (move_node_upward(this.bookmarks, cursor, emptynode)) {
-      this.saveBookmarks();
+      this.saveNodes();
     }
   }
 
-  movedownwardBookmark(cursor) {
+  movedownwardNode(cursor) {
     const emptynode = {type: 'bookmark', title: "InvisibleElement", content: "InvisibleContent", visible: 0};
     if (move_node_downward(this.bookmarks, cursor, emptynode)) {
-      this.saveBookmarks();
+      this.saveNodes();
     }
   }
 
-  deleteBookmark(cursor) {
+  deleteNode(cursor) {
     if (delete_node(this.bookmarks, cursor)) {
-      this.saveBookmarks();
+      this.saveNodes();
       this.forceUpdate();
     }
   }
 
-  importBookmarksReaderOnload(e) {
+  importNodesReaderOnload(e) {
     let newBookmarks = [];
     let text_error_loadingfile = this.i18n.text['text_error_loadingfile'];
     parse(e.target.result,
@@ -295,12 +233,12 @@ class BookmarksList extends React.Component {
         this.bookmarks.push(newBookmarks[i]);
       }
       // Save and display.
-      this.saveBookmarks();
+      this.saveNodes();
       this.forceUpdate();
     }
   }
 
-  importBookmarks(e) {
+  importNodes(e) {
     let file = e.target.files[0];
     if (!file) {
       if (e.target.files.length > 0) {
@@ -309,11 +247,11 @@ class BookmarksList extends React.Component {
       return;
     }
     let reader = new FileReader();
-    reader.onload = this.importBookmarksReaderOnload;
+    reader.onload = this.importNodesReaderOnload;
     reader.readAsText(file);
   }
 
-  exportBookmarksHelper(bookmarks) {
+  exportNodesHelper(bookmarks) {
     let bookmarksCount = bookmarks.length;
     let netscapeBookmarks = [];
     for (let i = 0; i < bookmarksCount; i++) {
@@ -345,7 +283,7 @@ class BookmarksList extends React.Component {
     return netscapeBookmarks.join('\n');
   }
 
-  exportBookmarks() {
+  exportNodes() {
     let netscapeBookmarks = [
       '<!DOCTYPE NETSCAPE-Bookmark-file-1>',
       '<!-- This is an automatically generated file.',
@@ -357,7 +295,7 @@ class BookmarksList extends React.Component {
       '',
       '<DL><p>'
     ];
-    netscapeBookmarks.push(this.exportBookmarksHelper(this.bookmarks));
+    netscapeBookmarks.push(this.exportNodesHelper(this.bookmarks));
     netscapeBookmarks.push('</DL><p>');
     saveAs(new Blob([netscapeBookmarks.join('\n')], { type: "text/plain;charset=utf-8" }),
       'bookmarks-' + get_timestamp() + '.html');
@@ -368,7 +306,7 @@ class BookmarksList extends React.Component {
     for (let i = 0; i < this.state.bookmarks.length; i++) {
       if (this.bookmarks[i].visible !== 0) {
         bookmarksRepresentation.push(
-          <Bookmark
+          <Node
             id={i.toString()}
             key={'Bookmark' + i + ' ' + this.state.bookmarks[i].visible}
             type={this.state.bookmarks[i].type}
@@ -379,19 +317,20 @@ class BookmarksList extends React.Component {
             showedit={this.showedit}
             showmove={this.showmove}
             showadd={this.showadd}
-            addBookmark={this.addBookmark}
-            editBookmark={this.editBookmark}
-            movebackwardBookmark={this.movebackwardBookmark}
-            moveforwardBookmark={this.moveforwardBookmark}
-            moveupwardBookmark={this.moveupwardBookmark}
-            movedownwardBookmark={this.movedownwardBookmark}
+            addNode={this.addNode}
+            openNode={this.openNode}
+            editNode={this.editNode}
+            movebackwardNode={this.movebackwardNode}
+            moveforwardNode={this.moveforwardNode}
+            moveupwardNode={this.moveupwardNode}
+            movedownwardNode={this.movedownwardNode}
             text={this.i18n.text}
           />);
       }
     }
     return (
       <AppWithTopBar refprop={this.bookmarksListRef} lang={this.i18n.language} appname={this.i18n.text['text_appname']}
-      icons={[{label: this.i18n.text['text_add_label'], icon: 'add', callback: () => this.addBookmark()},
+      icons={[{label: this.i18n.text['text_add_label'], icon: 'add', callback: () => this.addNode()},
               {label: this.i18n.text['text_settings_label'], icon: 'settings', callback: () => this.openSettings()},
               {label: this.i18n.text['text_importexport_label'], icon: 'import_export', callback: () => open_dialog(this.bookmarksListRef, 'impexp')},
               {label: this.i18n.text['text_help_label'], icon: 'help', callback: () => open_dialog(this.bookmarksListRef, 'help')},
@@ -400,7 +339,7 @@ class BookmarksList extends React.Component {
           {bookmarksRepresentation}
         </section>
           <EditDialog id="EditDialog" ref={this.EditDialogRef} text={this.i18n.text}
-           deleteBookmark={this.deleteBookmark} handleSubmit={this.handleSubmit} />
+           deleteNode={this.deleteNode} handleSubmit={this.handleSubmit} />
           <SettingsDialog id="SettingsDialog" ref={this.SettingsDialogRef} text={this.i18n.text}
            showedit={this.showedit} showmove={this.showmove} showadd={this.showadd} language={this.i18n.language} 
            handleSettingsChange={this.handleSettingsChange} />
@@ -408,9 +347,9 @@ class BookmarksList extends React.Component {
                   actions={(<span>
                     <label>{this.i18n.text['text_import']}
                     &nbsp;
-                    <input type="file" onChange={e => this.importBookmarks(e)} className="mdc-button mdc-dialog__button" data-mdc-dialog-action="yes" /></label>
+                    <input type="file" onChange={e => this.importNodes(e)} className="mdc-button mdc-dialog__button" data-mdc-dialog-action="yes" /></label>
                     <input type="submit" value={this.i18n.text['text_back'] || "Back"} className="mdc-button mdc-dialog__button" data-mdc-dialog-action="yes" />
-                    <input type="submit" value={this.i18n.text['text_export'] || "Export"} onClick={event => this.exportBookmarks()} className="mdc-button mdc-dialog__button" data-mdc-dialog-action="yes" /></span>)} >
+                    <input type="submit" value={this.i18n.text['text_export'] || "Export"} onClick={event => this.exportNodes()} className="mdc-button mdc-dialog__button" data-mdc-dialog-action="yes" /></span>)} >
             <p>{this.i18n.text['text_importexport_content']}</p>
           </Dialog>
           <Dialog id="help" title={this.i18n.text['text_help_title']} text_close_button={this.i18n.text['text_close_button']} >
@@ -439,7 +378,7 @@ class BookmarksList extends React.Component {
 function App() {
   return (
     <div className="App">
-      <BookmarksList />
+      <NodesList />
     </div>
   );
 }
